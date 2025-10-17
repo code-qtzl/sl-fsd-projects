01e67d0654e21e470572cd9930890a3e89ebfab8
code-qtzl <azepeda90@outlook.com>
2025-10-17 16:53:05 -0500

    initial commit: project setup

---
+++ recipe-app/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+	return (
+		<>
+			<h1 class='text-3xl font-bold text-blue-500 text-red-300'>
+				Hello Tailwind!
+			</h1>
+		</>
+	);
+}
+
+export default App;
