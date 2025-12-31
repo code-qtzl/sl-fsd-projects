630de6c3940b2d97d697e7e748e30de94cbbc4f1
code-qtzl <azepeda90@outlook.com>
2025-12-30 18:00:16 -0600

    [WIP] project removal

--- ems-project/model/loginModel.js
+++
@@ -1,9 +0,0 @@
-// map to collection in mongo db 
-class Login {
-    constructor(emailId,password){
-        this.emailId =emailId;
-        this.password=password;
-    }
-}
-
-module.exports =Login;
